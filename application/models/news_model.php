a317e4eebc3fa6b2eaca5d6bb4ef7e3330b13fde
Yunao Liu <yunao.liu1990@gmail.com>
2014-03-30 14:47:44 -0400

    Oracel配置完毕

--- application/models/news_model.php
+++ application/models/news_model.php
@@ -6,17 +6,15 @@ class News_model extends CI_Model {
     $this->load->database();
   }
   
-  public function get_news($slug = FALSE){
-  if ($slug === FALSE)
-  {
-    $query = $this->db->get('DISHES'); // changed from news to dishes
-    return $query->result_array();
-  }
-  
-  $query = $this->db->get_where('dishes', array('dishname' => $slug)); // changed from news to dishes
-  return $query->row_array();
-}
-  
-  
-  
+	public function get_news($slug = FALSE)
+	{
+		if ($slug === FALSE)
+		{
+		$query = $this->db->get('news');
+		return $query->result_array();
+		}
+
+		$query = $this->db->get_where('news', array('slug' => $slug));
+		return $query->row_array();
+	}
 }
--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,9 +1,16 @@
 <?php foreach ($news as $news_item): ?>
 
-    <h2><?php echo $news_item['title'] ?></h2>
+    <h2><?php echo $news_item['TITLE'] ?></h2>
+	<?php
+	/*
+		echo '<pre>';
+		print_r ($news_item);
+		echo '</pre>';
+	*/
+	?>
     <div id="main">
-        <?php echo $news_item['text'] ?>
+        <?php echo $news_item['TEXT'] ?>
     </div>
-    <p><a href="http://localhost/IDB/index.php/news/<?php echo $news_item['dishname'] ?>">View article</a></p>
+    <p><a href="http://localhost/IDB/index.php/news/<?php echo $news_item['SLUG'] ?>">View article</a></p>
 
 <?php endforeach ?>
